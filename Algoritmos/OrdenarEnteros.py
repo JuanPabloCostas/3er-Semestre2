c243a0dfaa636eda7159e5b1189c49ba26680caf
JuanPabloCostas <jcostas29@alumnos.uaq.mx>
2022-08-18 19:24:01 -0500

    bruhhhhhh

---
+++ Algoritmos/OrdenarEnteros.py
@@ -0,0 +1,22 @@
+#Algoritmo para ordenar de menor a mayor numeros en una lista
+#Juan Pablo Costas
+#Javier Valencia Vázquez
+#Juan Carlos Salas Gallegos
+#Jesús Alberto Vázquez Rioja
+#Grupo 30
+
+def Ordenar(vector):
+    for c in range(0,len(vector)):
+        pivote=c
+        for i in range(c,len(vector)):
+            if vector[i] < vector[pivote]:
+                aux = vector[i]
+                vector[i] = vector[pivote]
+                vector[pivote] = aux
+        print(vector)
+    return vector
+            
+
+vector = [8,11,3,29,18,10,22,7]
+print(vector)
+Ordenar(vector)
--- Pruebas.py
+++ Pruebas.py
@@ -3,15 +3,15 @@
 #JuanPaCostas
 import random
 
-a = random.randint(-50,50)
-b = random.randint(-50,50)
-c = random.randint(-50,50)
-d = random.randint(-50,50)
-e = random.randint(-50,50)
-f = random.randint(-50,50)
-g = random.randint(-50,50)
-h = random.randint(-50,50)
-i = random.randint(-50,50)
+a = random.uniform(-500, 500)
+b = random.uniform(-500, 500)
+c = random.uniform(-500, 500)
+d = random.uniform(-500, 500)
+e = random.uniform(-500, 500)
+f = random.uniform(-500, 500)
+g = random.uniform(-500, 500)
+h = random.uniform(-500, 500)
+i = random.uniform(-500, 500)
 
 derivadas = []
 derivadas.append(a*8)
@@ -23,11 +23,12 @@ derivadas.append(f*3)
 derivadas.append(g*2)
 derivadas.append(h*1)
 
-
-for k in range(0,10):
-    x = random.randint(0,10000)
-    print("X= ",x)
+raices = []
+oldraiz = None
+while len(raices) < 5:
+    x = random.uniform(-500000, 500000)
     i = 1
+    raiz = None
     oldx = None
     while i > 0:
         fx = a*x**8 + b*x**7 + c*x**6 + d*x**5 + e*x**4 + f*x**3 + g*x**2 + h*x**1 + i
@@ -35,6 +36,33 @@ for k in range(0,10):
         x = x - fx/fxd        
         if oldx == x:
             i = 0
+            raiz = x
         oldx = x
-    print("raiz= ",x)
+    stop = False
+    for k in range(0,len(raices)):
+        if raices[k] == raiz:
+            stop = True
+            break
+    if stop == False:
+        raices.append(raiz)
+        print("Raiz",len(raices),"= ",raiz)
+    oldraiz = raiz
+print("Se acabo")
     
+
+    
+def Ordenar(vector,pivote):
+    for i in range (0, len(vector)):
+        if vector[i] > vector[pivote]:
+            aux = vector[pivote]
+            vector[pivote] = vector[i]
+            vector[i] = aux
+    return vector
+
+
+vector = [8,11,3,29,18,10,22,7]
+print(vector)
+for k in range (0, len(vector)):
+    pivote = k
+    vector = Ordenar(vector, pivote)
+    print(vector)
