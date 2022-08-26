437f8fa26716d68bc9b4f794c84680e704d2010d
JuanPabloCostas <jcostas29@alumnos.uaq.mx>
2022-08-25 23:21:58 -0500

    triangulo de pascal

---
+++ CalculoDiferencialeIntegral/BinomiosNewton.py
@@ -0,0 +1,38 @@
+import abc
+import os
+os.system('cls||clear')
+
+# ex = int(input("Ingresa el exponente> "))
+exGeneral = 8
+
+ecuaciones = []
+
+for k in range(0,exGeneral+1):
+    ecuaciones.clear()
+
+    constante = 1
+    exA = k
+    exB = 0
+    for i in range(0,k+1):
+        auxC= str(int(constante))
+
+        if exA == 0:
+            auxA = ""
+        elif exA == 1:
+            auxA = "a"
+        else:
+            auxA = "a^"+str(exA)
+
+        if exB == 0:
+            auxB = ""
+        elif exB == 1:
+            auxB = "b"
+        else:
+            auxB = "b^"+str(exB)
+
+        ecuaciones.append(auxC)
+        constante = constante * exA / (exB+1)
+        exA = exA-1
+        exB = exB+1
+    ej = (str(ecuaciones)).replace(",","").center(120)
+    print(ej) 
