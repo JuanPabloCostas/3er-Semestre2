213c93734031e1008100dcbdbf9e151d45697989
JuanPabloCostas <jcostas29@alumnos.uaq.mx>
2022-09-02 09:51:14 -0500

    pruebas button

--- CalculoDiferencialeIntegral/leydesignos.py
+++ CalculoDiferencialeIntegral/leydesignos.py
@@ -1,7 +1,8 @@
 import os
 os.system('cls||clear')
 
-str = input("Ingrese la ecuacion> ")
+# str = input("Ingrese la ecuacion> ")
+str = "x^4+x^3+x^2+x+1"
 alt = str
 
 alt = alt.replace("+", "@")
@@ -82,20 +83,62 @@ def cambiosDeSignoNeg(inputs, integers):
         oldState = currentState
     return cantcambios
 
-i = cambiosDeSigno(inputs)
+# i = cambiosDeSigno(inputs)
 pos = cambiosDeSigno(inputs)
 neg = cambiosDeSignoNeg(inputs, integers)
-img = integers[0]-(pos+neg)
-alt = i
+i = pos
+k = neg
+img = integers[0]
 listado = []
-print("Postivos     Negativos   Imaginarios")
 while i >= 0:
-    listado.append(pos-(alt-i))
-    listado.append(neg)
-    listado.append(img+(alt-i))
+    listado.append(i)
+    listado.append(k)
+    listado.append(img-(i+k))
     print(listado)
     listado.clear()
-    i = i - 2
+    if(i == 0 or i == 1):
+        break
+    else:
+        i = i - 2
+
+while k >= 0:
+    if(k == 0 or k == 1):
+        break
+    else:
+        k = k - 2
+    listado.append(i)
+    listado.append(k)
+    listado.append(img-(i+k))
+    print(listado)
+    listado.clear()
+    
+
+# img = integers[0]-(pos+neg)
+# alt = i
+# listado = []
+# print("Postivos     Negativos   Imaginarios")
+# while i >= 0:
+#     listado.append(pos-(alt-i))
+#     listado.append(neg)
+#     listado.append(img+(alt-i))
+#     print(listado)
+#     k = listado[1]
+#     listado.clear()
+#     i = i - 2
+# print(k)
+# while k >= 0:
+#     k = k -2
+#     listado.append(i)
+#     listado.append(k)
+#     listado.append(img+(k-i))
+    
+#     print(listado)
+#     listado.clear()
+
+
+
+
+    # 8x^8-7x^6+2x^4+4x^3+5^2-2x-8
 
 
 
--- Pruebas.py
+++ Pruebas.py
@@ -4,10 +4,19 @@ from tkinter import *
 # Were creating the main window
 root = Tk()
 
-# Creating a Label Widget
-myLabel = Label(root, text="hello world!")
-# Shoving it onto the screen
-myLabel.pack()
+myButton = Button(root, text="Click me", padx=50, pady=50)
+myButton.pack()
+
+
+
+
+# # Creating a Label Widget
+# myLabel1 = Label(root, text="hello world!")
+# myLabel2 = Label(root, text="Juan Pablo Costas")
+
+# # Shoving it onto the screen
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=5)
 
 
 root.mainloop()
